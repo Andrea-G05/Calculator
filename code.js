85758cc70c243164a53492efc8027b80f09c75ae
Andrea-G05 <andreagiannotti9105@gmail.com>
2023-07-30 16:47:32 +0200

    Add all operation and clear

--- code.js
+++ code.js
@@ -1,15 +1,16 @@
-let num1 = 0;
-let num2 = 0;
+let num1 = "0";
+let num2 = "0";
 let oper;
+
+let display = document.querySelector(".display p");
 
 document.querySelectorAll("button").forEach( (button)=>
 	button.addEventListener("click", () => elaborate(button.dataset.input)));
 
-
-	function elaborate (input){
-	if(input.match(/[0-9]/))
+function elaborate (input) {
+	if (input.match(/[0-9]/))
 		inputNumber(input)
-	else if(input.match(/[\+\-\x\/]/))
+	else if (input.match(/[\+\-\x\/]/))
 		inputOperator(input)
 	else switch(input){
 		case "=":
@@ -29,26 +30,86 @@ document.querySelectorAll("button").forEach( (button)=>
 	}
 }
 
-function inputNumber(num){
-	console.log("num")
+function inputNumber(input) {
+	input = input.toString();
+	if (oper === undefined){
+		num1 === "0"?
+		num1 = input:
+		num1 += input;
+		display.textContent = num1;
+	} else {
+		num2 === "0"?
+		num2 = input:
+		num2 += input;
+		display.textContent = num2;
+	}
 }
 
-function inputOperator(oper){
-	console.log("oper")
+function inputOperator(input) {
+	if (oper === undefined){
+		oper = input; 
+	} else {
+		num1 = inputEqual();
+		oper = input;
+	}
 }
 
-function inputEqual(){
-	console.log("equal")
+function inputEqual() {
+	if (oper === undefined)
+		return;
+	let result;
+	switch (oper){
+		case "+":
+			result = add(num1, num2);
+			break;
+		case "-":
+			result = subtract(num1, num2);
+			break;	
+		case "x":
+			result = multiply(num1, num2);
+			break;
+		case "/":
+			result = divide(num1, num2);
+			break;
+	}
+	display.textContent = result;
+	num1 = "0";
+	num2 = "0";
+	oper = undefined;
+	return result;
 }
 
-function inputClear(){
-	console.log("clear")
+function inputClear() {
+	display.textContent = "0";
+	num1 = "0";
+	num2 = "0";
+	oper = undefined;
 }
 
-function inputDot(){
+function inputDot() {
 	console.log("dot")
 }
 
-function inputSign(){
+function inputSign() {
 	console.log("sign")
+}
+
+function add(a,b) {
+	let result = +a + +b;
+	return result.toString();
+}
+
+function subtract(a,b) {
+	let result = +a - +b;
+	return result.toString();
+}
+
+function multiply(a,b) {
+	let result = +a * +b;
+	return result.toString();
+}
+
+function divide(a,b) {
+	let result = +a / +b;
+	return result.toString();
 }
